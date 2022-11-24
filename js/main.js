14880744cae7b4559badbf5bd89bf0b64dc2562d
Артем Горбаль <homerion@MacBook-Air-Artem.local>
2022-11-24 14:32:25 +0300

    section modal-request

--- js/main.js
+++ js/main.js
@@ -35,4 +35,49 @@ $(document).ready(function () {
         $('.header__content').addClass('active');
     });
 
+    // Modal
+    $('.modal-request__form').iziModal({
+        overlayColor: 'rgba(0, 3, 21, 0.5)',
+        width: '108rem',
+        zindex: 30,
+        group: 'request',
+        transitionIn: 'fadeInUp',
+        transitionOut: 'comingOut',
+        navigateArrows: false,
+        navigateCaption: false,
+        onOpening: function() {
+            $('body').width($('body').width()).addClass('lock');
+        },
+        onClosing: function() {
+            $('body').width('').removeClass('lock');
+        },
+    });
+    $('.modal-request__complete').iziModal({
+        overlayColor: 'rgba(0, 3, 21, 0.5)',
+        width: '69rem',
+        zindex: 30,
+        group: 'request',
+        transitionIn: 'fadeInUp',
+        transitionOut: 'comingOut',
+        navigateArrows: false,
+        navigateCaption: false,
+        onOpening: function() {
+            $('body').width($('body').width()).addClass('lock');
+        },
+        onClosing: function() {
+            $('body').width('').removeClass('lock');
+        },
+    });
+    $('.modal-request__form-footer .button').click(function () {
+        $('.modal-request__form').iziModal('next');
+    });
+    $('.popup-request').click(function () {
+        $('.modal-request__form').iziModal('open');
+    });
+    $('.modal-request__label input, .modal-request__label-textarea textarea').focusin(function () {
+        $(this).parent().addClass('focus');
+    }).focusout(function () {
+        $(this).parent().removeClass('focus');
+    });
+
 });
